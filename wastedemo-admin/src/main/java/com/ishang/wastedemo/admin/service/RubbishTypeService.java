4064e7f5c515255e75e4cc58647cea0ee07c251a
Asterisk Azurain <qysx32_kitty@yeah.net>
2020-04-18 09:09:22 +0800

    完成了先行功能点，完成了大多数模块的基础代码。todo:需要考虑del_flag的显示问题(sql)

--- wastedemo-admin/src/main/java/com/ishang/wastedemo/admin/service/RubbishTypeService.java
+++ wastedemo-admin/src/main/java/com/ishang/wastedemo/admin/service/RubbishTypeService.java
@@ -1,8 +1,15 @@
 package com.ishang.wastedemo.admin.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
 import com.ishang.wastedemo.admin.entity.RubbishType;
 import com.ishang.wastedemo.core.service.CurdService;
 
+@Service
 public interface RubbishTypeService extends CurdService<RubbishType> {
+
+	List<RubbishType> findall();
 
 }
